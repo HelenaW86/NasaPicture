7440aa0f59968362a69c7c1f17efe0e1062f2929
Helena Wiklund <helena.wiklund@live.se>
2024-03-14 10:42:49 +0100

    local

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ import express from 'express';
 import axios from 'axios';
 
 const app = express();
-const port = 8080;
-app.set('view engine', 'ejs');
+const port = 3000;
 
 app.use(express.static("public"));
 app.get("/", async (req, res) => {
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 0);
+    const yesterdayDate = yesterday.toISOString().split('T')[0];
     try {
-        const picture = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+        const picture = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}&date=${yesterdayDate}`)
         console.log(picture.data)
         res.render("index.ejs", { url: picture.data.hdurl, mediaType: picture.data.media_type, videourl: picture.data.url, title: picture.data.title, explanation: picture.data.explanation })
     } catch (error) {
